5d1335a6e8fb8a160371443fa2357486a3b12ea6
Hoon <yimsh4507@gmail.com>
2022-09-21 19:21:52 -0400

    Lab3

--- 101325908_COMP3123-exec03/index.js
+++ 101325908_COMP3123-exec03/index.js
@@ -1,11 +1,13 @@
-var http = require("http");
+const http = require("http");
+const Employees = require("./Employee");
+
 //TODO - Use Employee Module here
 console.log("Lab 03 -  NodeJs");
 
 //TODO - Fix any errors you found working with lab exercise
 
 //Define Server Port
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 3000
 
 //Create Web Server using CORE API
 const server = http.createServer((req, res) => {
@@ -14,22 +16,33 @@ const server = http.createServer((req, res) => {
     } else {
         if (req.url === '/') {
             //TODO - Display message "<h1>Welcome to Lab Exercise 03</h1>"
+            res.write("<h1>Welcome to Lab Exercise 03</h1>");
         }
-
         if (req.url === '/employee') {
             //TODO - Display all details for employees in JSON format
+            // res.write(JSON.stringify(Employee))
+            res.setHeader("Content-Type", "application/json");
+            for (const employee of Employees) {
+                res.write(JSON.stringify(employee, null, 4) + "\n");
+            }
         }
 
         if (req.url === '/employee/names') {
             //TODO - Display only all employees {first name + lastname} in Ascending order in JSON Array
             //e.g. [ "Ash Lee", "Mac Mohan", "Pritesh Patel"]
+            const arr = Employees.map(employee => employee.firstName + ' ' + employee.lastName);
+            // res.write(`${arr.sort()}`);
+            res.write(JSON.stringify(arr.sort()));
         }
 
         if (req.url === '/employee/totalsalary') {
             //TODO - Display Sum of all employees salary in given JSON format 
             //e.g. { "total_salary" : 100 }  
-    }
-    res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+            const sum = Employees.reduce((last, employee) => employee.Salary + last, 0);
+            res.write(JSON.stringify({ total_salary: sum }));
+        }
+        // res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+        res.end();
     }
 })
 
