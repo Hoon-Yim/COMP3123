f838f9f533fa0d4d942f0f29b04b1fdb16d1534f
Hoon Yim <yimsh4507@gmail.com>
2022-11-01 17:52:09 -0400

    lab7

---
+++ 101325908_COMP3123-exec07/react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - 1</h1>
+        <h3>React JS Programming Week09 Lab exercise</h3>
+        <h4>101325908</h4>
+        <h5>Hoon Yim</h5>
+        <h6>George Brown College, Toronto</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
